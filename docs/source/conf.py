eaf61c7e05780fedccfdded392a436650c2d691d
hiroakif <hiroakif1637@gmail.com>
2025-03-12 13:43:02 +0900

    pyproject.optional-dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,3 +25,10 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+[pyproject.optional-dependencies]
+docs = [
+    "sphinx",
+    "sphinx_rtd_theme",
+]
