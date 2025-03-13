5368f0c867ce6440f3ec089038b682e09a0571e2
hiroakif <hiroakif1637@gmail.com>
2025-03-13 13:38:44 +0900

    extenyions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,12 +23,12 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
+]
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-
-[project.optional-dependencies]
-docs = [
-    "sphinx",
-    "sphinx_rtd_theme",
-]
