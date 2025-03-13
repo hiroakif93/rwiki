58d89ed023689330238f8ed6982c263f03f81a7e
hiroakif <hiroakif1637@gmail.com>
2025-03-13 13:44:43 +0900

    upgrade

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,6 @@ author = 'hiroakif93'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
 
